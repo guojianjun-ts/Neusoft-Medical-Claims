2e0807ab521fdc07a9ab2f22b579e883383c0347
guojianjun-ts <785689178@qq.com>
2025-07-08 10:45:27 +0800

    后端初始化

---
+++ NMC-Backend/src/main/java/com/gjj/nmcbackend/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.gjj.nmcbackend.controller;
+
+import com.gjj.nmcbackend.common.BaseResponse;
+import com.gjj.nmcbackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
---
+++ NMC-Backend/src/test/java/com/gjj/nmcbackend/NmcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjj.nmcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
